4a0eb5903f71ed3d448332c23334c3f84e61e264
Lysgren <oscar.lysgren@gmail.com>
2020-11-02 15:43:56 +0100

    Rewrote

--- writtingScript.js
+++ writtingScript.js
@@ -8,41 +8,57 @@ let getData = async requestedData => {
   return data
 }
 
-let createDetails = item => {
-  let details = document.querySelector("div.character")
+let writeDataInDOM = (position, item) => {
   let data = document.createElement("div");
   data.innerHTML = item
-  details.append(data)
+  position.append(data)
+}
+
+let writePlanet = async (planet) => {
+  let planetDOM = document.querySelector("div.planet")
+  planetDOM.innerHTML = ""
+
+  let planetData = await getData(planet)
+  writeDataInDOM(planetDOM, "Planet name: " + planetData.name)
+  writeDataInDOM(planetDOM, "Rotation period: " + planetData.rotation_period)
+  writeDataInDOM(planetDOM, "Orbital period: " + planetData.orbital_period)
+  writeDataInDOM(planetDOM, "Diameter: " + planetData.diameter)
+  writeDataInDOM(planetDOM, "Climate: " + planetData.climate)
+  writeDataInDOM(planetDOM, "Gravity: " + planetData.gravity)
+  writeDataInDOM(planetDOM, "Terrain: " + planetData.terrain)
 }
 
 let writeCharacter = character => {
-  let nameList = document.querySelector(".characters-names")
-  let btn = document.createElement("button");
-  btn.innerHTML = character.name
-  btn.classList.add("char-box");
-  nameList.append(btn)
+  let detailsDOM = document.querySelector("div.character")
+  detailsDOM.innerHTML = ""
 
-  btn.addEventListener("click", function() {
-    document.querySelector("div.character").innerHTML = ""
-    createDetails(character.name)
-    createDetails("Height: " + character.height + " cm")
-    createDetails("Mass: " + character.mass + " kg")
-    createDetails("Hair colour: " + character.hair_color)
-    createDetails("Skin colour: " + character.skin_color)
-    createDetails("Eye colour: " + character.eye_color)
-    createDetails("Birth year: " + character.birth_year)
-    createDetails("Gender " + character.gender)
-  });
-
+  writeDataInDOM(detailsDOM, character.name)
+  writeDataInDOM(detailsDOM, "Height: " + character.height + " cm")
+  writeDataInDOM(detailsDOM, "Mass: " + character.mass + " kg")
+  writeDataInDOM(detailsDOM, "Hair colour: " + character.hair_color)
+  writeDataInDOM(detailsDOM, "Skin colour: " + character.skin_color)
+  writeDataInDOM(detailsDOM, "Eye colour: " + character.eye_color)
+  writeDataInDOM(detailsDOM, "Birth year: " + character.birth_year)
+  writeDataInDOM(detailsDOM, "Gender " + character.gender)
+  
+  writePlanet(character.homeworld)
 }
 
 let renderCharacterList = async currentPage => {
   document.querySelector(".characters-names").innerHTML = ""
-  document.querySelector(".page-number").innerHTML = currentPage + "/8"
+  document.querySelector(".page-number").innerHTML = currentPage + "/9"
 
   let data = await getData("https://swapi.dev/api/people/?page=" + currentPage)
-  for (let i = 0; i < data.results.length - 4; i++) {
-    writeCharacter(data.results[i])
+  for (let i = 0; i < data.results.length; i++) {
+    let nameList = document.querySelector(".characters-names")
+    let btn = document.createElement("button");
+    btn.innerHTML = data.results[i].name
+    btn.classList.add("char-box");
+    nameList.append(btn)
+  
+    btn.addEventListener("click", function() {
+      writeCharacter(data.results[i])
+    });
   }
 }
 
@@ -50,27 +66,13 @@ let main = async () => {
   let currentPage = 1
 
   document.querySelector("button.arrowLeft").addEventListener("click", function() {
-    currentPage--
-
-    if (currentPage == 0) {
-      currentPage = 8
-      renderCharacterList(currentPage)
-    
-    } else {
-      renderCharacterList(currentPage)
-    }
+    currentPage = (currentPage == 1) ? 9 : currentPage -= 1
+    renderCharacterList(currentPage)
   });
   
   document.querySelector("button.arrowRight").addEventListener("click", function() {
-    currentPage++
-
-    if (currentPage == 9) {
-      currentPage = 1
-      renderCharacterList(currentPage)
-    
-    } else {
-      renderCharacterList(currentPage)
-    }
+    currentPage = (currentPage == 9) ? 1 : currentPage += 1
+    renderCharacterList(currentPage)
   });
 
   renderCharacterList(currentPage)
